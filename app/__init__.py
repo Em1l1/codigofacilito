d0399a3a5720a07e655ca33ca866a0c7b256626f
victoriano <mytacam78@gmail.com>
2023-03-08 01:30:39 -0600

    redirection index and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,9 +6,18 @@ app = Flask(__name__)
 def index():
   return render_template('index.html') 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  return render_template('auth/login.html')
+  # print(request.method)
+  if request.method == 'POST':
+    if request.form['usuario'] == 'admin' and request.form['password'] == '12345':
+    # print(request.form['usuario'])
+    # print(request.form['password'])
+      return redirect(url_for('index'))
+    else:
+      return render_template('auth/login.html')
+  else:
+    return render_template('auth/login.html')
 
 def pagina_no_encontrada(error):
   return render_template('errors/404.html'), 404
