008998119015144839cd42d0557e05e026dfe093
victoriano <mytacam78@gmail.com>
2023-03-10 11:48:50 -0600

    command ls

---
+++ unix/1_first_command.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+touch touch.txt
+nvim touch.txt
+
+#head
+head frankenstein.txt
+
+head -n 25 frankenstein.txt
+
+# tail
+tail frankenstein.txt
+
+tail -n 15 frankenstein.txt
+
+# wc
+wc frankenstein.txt
+wc -c frankenstein.txt
+wc -w frankenstein.txt
+wc -l frankenstein.txt
---
+++ unix/2_ls.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ls
+
+ls -a
+
+ls -l
+
+ls -la
+
+# listar en modo reverso
+ls -lr
+
+ls -ltr
+
+# extension mayuscula
+ls -ltX
+
+ls -ltXr
+
+ls -lS
+
+ls -lSr
+
+ls -lSrh
