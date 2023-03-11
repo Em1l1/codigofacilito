d88bbbc225dbd33ef27b889dc280beeeeccb8b37
victoriano <mytacam78@gmail.com>
2023-03-11 17:26:36 -0600

    crontab

---
+++ unix/16_pipe.sh
@@ -0,0 +1,7 @@
+tail frankenstein.txt | wc
+
+tail frankenstein.txt | grep he
+
+tail frankenstein.txt | grep -w he
+
+tail frankenstein.txt | grep -w he | wc 
---
+++ unix/17_cron.sh
@@ -0,0 +1,6 @@
+# cron
+# editar el archivo crontrab
+crontrab -e
+
+
+*/5 * * * * * echo "Hola, nos encontramos en el archivo crontrab" << /home/codigofacilito/archivos/crontab.txt
